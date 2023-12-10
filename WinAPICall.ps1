b9ffd71260a96afe9f69663455cfebc6dfb348fb
Rio Asmara Suryadi <rio.asmara@gmail.com>
2023-12-10 19:55:48 +0300

    Update WinAPICall.ps1

--- WinAPICall.ps1
+++ WinAPICall.ps1
@@ -1,3 +1,4 @@
 Add-Type -AssemblyName PresentationFramework
 [System.Windows.MessageBox]::Show('Message Box Called')
 [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Calling winAPI by Powershell')
+[System.Text.Encoding]::Unicode.GetString([convert]::FromBase64String("IFtTeXN0ZW0uV2luZG93cy5Gb3Jtcy5NZXNzYWdlQm94XTo6U2hvdygnQ2FsbGluZyB3aW5BUEkgYnkgUG93ZXJzaGVsbCcp"))
